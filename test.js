870d3b38ab006f1be62431df70e41c9117d135fe
Worawut Boontan <worawut@theiconweb.com>
2020-12-06 04:58:13 +0700

    Remove Test

--- test.js
+++
@@ -1,7 +0,0 @@
-const dayjs = require('dayjs');
-var customParseFormat = require('dayjs/plugin/customParseFormat')
-dayjs.extend(customParseFormat)
-var utc = require('dayjs/plugin/utc')
-dayjs.extend(utc)
-
-console.log(dayjs().utc().toDate())
